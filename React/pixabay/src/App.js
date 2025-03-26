5b7718c44ae2c82f255138d04e3de39898feb3f0
t-keisuke-0 <kasakokey0728@gmail.com>
2025-03-26 22:30:13 +0900

    pixabay API

---
+++ React/pixabay/src/App.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import SearchBar from './components/SearchBar';
+import ImageList from './components/imageList';
+import logo from './pixabaylogo.png';
+
+const App = () => {
+  const [images, setImages] = useState([]);
+  const ApiKey = process.env.REACT_APP_PIXABAY_APIKEY;
+  const onSearchSubmit = async (term) => {
+    try {
+      const params = {
+        key: ApiKey,
+        q: term,
+        lang: 'ja',
+      };
+      const response = await axios.get('https://pixabay.com/api/', { params });
+      setImages(response.data.hits);
+      if (response.data.total === 0) {
+        window.alert('お探しの画像は有りません。');
+      }
+    } catch {
+      window.alert('写真の取得に失敗しました。');
+    }
+  };
+
+  return (
+    <div className="ui container" style={{ marginTop: '20px' }}>
+      <img src={logo} alt="pixabay-logo" className="pixabay-logo" />
+      <SearchBar onSubmit={onSearchSubmit} />
+      <ImageList images={images} />
+    </div>
+  );
+};
+
+export default App;
